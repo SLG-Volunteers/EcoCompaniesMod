32ae233d8ba075a6af69a5d7be0bdf8cd7353757
Thomas Smith <thomasfn@fortfn.co.uk>
2024-09-12 20:05:34 +0100

    Added mod registration.

---
+++ EcoCompaniesMod/Registration.cs
@@ -0,0 +1,14 @@
+﻿namespace Eco.Mods.Companies
+{
+    using Core.Plugins.Interfaces;
+
+    public class CompaniesMod : IModInit
+    {
+        public static ModRegistration Register() => new()
+        {
+            ModName = "Companies",
+            ModDescription = "Extends the law and economy system with player controllable companies.",
+            ModDisplayName = "Companies",
+        };
+    }
+}
