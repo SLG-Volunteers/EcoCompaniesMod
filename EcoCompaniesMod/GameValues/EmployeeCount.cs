ec61a7dd2f990f5537cf93a592efcd14ec2da1c7
Kevin <kevin.boehme@gmail.com>
2026-03-03 18:01:15 +0100

    Refactor, Bugfixes and more...
    * Changed reputation calculation to only use +/- reputation and not
    doing any relative calculation
    * Force the deed creator to be `LegalPerson`.
    * AllowPlotsUnclaiming enabled by default.
    * Removed the Status and Configure commands (unuseful)
    * Added a Promote command for CEO transfer with confirmation (without
    voting, only CEO can use it)
    * Disabled some old debug output.
    * OnlyActive for EmployeeCount

--- EcoCompaniesMod/GameValues/EmployeeCount.cs
+++ EcoCompaniesMod/GameValues/EmployeeCount.cs
@@ -11,11 +11,14 @@
     using Gameplay.Civics.GameValues;
     using Gameplay.Systems.TextLinks;
     using Gameplay.Players;
+    using System;
+    using System.Linq;
 
     [Eco, LocCategory("Companies"), LocDescription("The number of employees of a company, including the CEO.")]
     public class EmployeeCount : GameValue<float>
     {
         [Eco, Advanced, LocDescription("The legal person whose company's employee count is being evaluated.")] public GameValue<User> LegalPerson { get; set; }
+        [Eco, Advanced, LocDescription("Whether to count only active employees.")] public GameValue<bool> OnlyActive { get; set; } = new No();
 
         private Eval<float> FailNullSafeFloat<T>(Eval<T> eval, string paramName) =>
             eval != null ? Eval.Make($"Invalid {Localizer.DoStr(paramName)} specified on {GetType().GetLocDisplayName()}: {eval.Message}", float.MinValue)
@@ -24,12 +27,13 @@
         public override Eval<float> Value(IContextObject action)
         {
             var legalPerson = this.LegalPerson?.Value(action); if (legalPerson?.Val == null) return this.FailNullSafeFloat(legalPerson, nameof(this.LegalPerson));
+            var onlyActive = this.OnlyActive?.Value(action).Val ?? false;
 
             var company = Company.GetFromLegalPerson(legalPerson.Val);
             if (company == null) return this.FailNullSafeFloat(legalPerson, nameof(this.LegalPerson));
-            float employeeCount = company.AllEmployees.Count();
+            float employeeCount = onlyActive == true ? company.AllEmployees.Count(e => !e.IsAbandoned) : company.AllEmployees.Count();
 
-            return Eval.Make($"{Text.StyledNum(employeeCount)} (employee count of {company.UILink()})", employeeCount);
+            return Eval.Make(new LocString($"{Text.StyledNum(employeeCount)} (" + (onlyActive ? $"active " : $"") + $"employee count of {company.UILink()})"), employeeCount);
         }
 
         public override LocString Description() => Localizer.Do($"employee count of company of {LegalPerson.DescribeNullSafe()}");
